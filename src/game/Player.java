27a76022af32e213d5372c71702982bd2f624bf5
Hongyi800 <hongyigu8@gmail.com>
2020-11-02 23:55:06 +0800

    first change

---
+++ src/game/Player.java
@@ -0,0 +1,69 @@
+package game;
+
+import java.util.ArrayList;
+
+import ch.aplu.jcardgame.*;
+
+public abstract class Player {
+	private Hand[] hands;
+	private Card select;
+	private int id;
+	private int type;
+	
+	public Card takeTurn() {
+		return select;
+	}
+	
+	public void printState() {
+		System.out.println("Player "+getId()+" play: "+getSelect().toString()+" from ["+printHand(hands[getId()].getCardList())+"]");
+	}
+
+	private String printHand(ArrayList<Card> cards) {
+		String out = "";
+		for(int i = 0; i < cards.size(); i++) {
+			out += cards.get(i).toString();
+			if(i < cards.size()-1) out += ",";
+		}
+		return(out);
+	}
+	
+	//get and set select card
+	public Card getSelect() {
+		return select;
+	}
+	public void setSelect(Card select) {
+		this.select = select;
+	}
+	
+	//get and set player id
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	
+	//get and set hand
+	public Hand[] getHands() {
+		return hands;
+	}
+
+	public void setHands(Hand[] hands) {
+		this.hands = hands;
+	}
+
+	//get and set player type
+	//0: interactive player
+	//1: normal NPC
+	//2: advanced NPC
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+	
+	
+}
